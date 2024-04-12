90b91151761b4ecb9302408fe1902dcf92cfffeb
Mike Chambers <mikechambers@gmail.com>
2024-04-12 09:04:06 -0700

    cleaning up formatting

--- scripts/rstyles.py
+++ scripts/rstyles.py
@@ -30,7 +30,10 @@ from firefly_lib import sanitize_filename, style_presets, run_firefly_command
 write_settings = False
 
 #these styles will not be used. You can override using --ignore-styles argument
-ignore_styles = ["newspaper_collage", "photographed_through_window"]
+ignore_styles = [
+    "newspaper_collage", "photographed_through_window", "stamp", 
+    "interior_design"
+]
 
 styles = style_presets
 
@@ -63,23 +66,34 @@ def generate_images(prompt, output_dir, num_images, num_styles_per_image, seed):
         n = sanitize_filename(n)
         filename = f"{n}.jpg"
 
-        run_command(prompt, output_dir, filename, selected_styles, seed)
+        try:
+            run_command(prompt, output_dir, filename, selected_styles, seed)
+        except Exception as e:
+            print(f"Caught an unexpected error calling firefly: {e}")
 
         time.sleep(10)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate images with random styles using the firefly command.")
-    parser.add_argument("--prompt", required=True, help="The prompt for the image generation.")
-    parser.add_argument("--output-dir", dest="output_dir", required=True, help="Directory where images will be saved.")
-    parser.add_argument("--num-images", dest="num_images", type=int, default=5, help="Number of images to create.")
-    parser.add_argument("--num-styles", dest="num_styles", type=int, default=5, help="Number of random styles to combine for each image.")
-    parser.add_argument("--seed", type=int, default=None, help="Seed to use to generate image. If not set, random seed will be used")
-    parser.add_argument('--ignore-styles', dest="ignore_styles", nargs='*', help='A list of styles that will not be used.')
-
-    parser.add_argument('--styles', dest="styles", nargs='+', help='A list of styles to pull random styles from.')
-
-    parser.add_argument('--write-settings', dest='write_settings', action='store_true', help='Disable backup.')
+    parser = argparse.ArgumentParser(
+        description="Generate images with random styles using the firefly command.")
+    parser.add_argument("--prompt", required=True, 
+                        help="The prompt for the image generation.")
+    parser.add_argument("--output-dir", dest="output_dir", required=True, 
+                        help="Directory where images will be saved.")
+    parser.add_argument("--num-images", dest="num_images", type=int, default=5, 
+                        help="Number of images to create.")
+    parser.add_argument("--num-styles", dest="num_styles", type=int, default=5, 
+                        help="Number of random styles to combine for each image.")
+    parser.add_argument("--seed", type=int, default=None, 
+                        help="Seed to use to generate image. If not set, random seed will be used")
+    parser.add_argument('--ignore-styles', dest="ignore_styles", nargs='*', 
+                        help='A list of styles that will not be used.')
+    parser.add_argument('--styles', dest="styles", nargs='+', 
+                        help='A list of styles to pull random styles from. Overrides built in style list.')
+    parser.add_argument('--write-settings', dest='write_settings', 
+                        action='store_true', 
+                        help='Write out a JSON file with settings used to generate image.')
     
     args = parser.parse_args()
 
